ef6cd80cedf9bf51f21a015f289ba779bace1692
Ben Conner <ben.conner40@gmail.com>
2016-12-05 14:42:25 -0600

    print out to page

--- seasonal-sales.js
+++ seasonal-sales.js
@@ -1,23 +1,36 @@
 var seasonSelect = document.getElementById("sale");
+var prodRequest = new XMLHttpRequest();
+var catRequest = new XMLHttpRequest();
+var prodHtml = "<ul>";
 
-
-
-var products = new XMLHttpRequest(),
-categories = new XMLHttpRequest(),
-
-
-function prolist(){
-  console.log(json.parse(products.responseText)
+function saleCalc(){
+  if(seasonSelect.value === "spring"){
+        console.log("enjoy a spring discount")
+  }else if(seasonSelect.value === "autumn"){
+    console.log("enjoy a fall discount")
+  }else if(seasonSelect.value === "winter"){
+    console.log("enjoy a winter discount")
+  }else{
+    console.log('its summer enjoy no discount but the outside!!!')
+  }
 }
 
-function catList(){
-  console.log(json.parse(categories.responseText)
+function loadUp(e){
+  var data = JSON.parse(e.target.responseText)
+
+  for (var i = 0; i < data.products.length; i++) {
+    prodHtml += `<li>${data.products[i].name}</li>`
+    console.log(prodHtml)
+  }
+  document.getElementById("prod").innerHTML=prodHtml;
 }
 
-products.addEventListener("load", proList);
-products.open("GET", "products.json");
-products.send()
+seasonSelect.addEventListener("change", saleCalc)
 
-categories.addEventListener("load", catList);
-categories.open('GET', 'categories.json');
-categories.send()
+prodRequest.addEventListener("load", loadUp);
+prodRequest.open("GET", "products.json");
+prodRequest.send()
+
+catRequest.addEventListener("load", loadUp);
+catRequest.open('GET', 'categories.json');
+catRequest.send()
